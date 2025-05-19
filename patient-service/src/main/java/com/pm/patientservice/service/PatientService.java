0fbc886e04f90140a58f1cb0ae1eb38d79fc8043
Michael Shaba <shabamichael@outlook.com>
2025-05-19 16:03:26 -0700

    Initial Commit

---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,41 @@
+package com.pm.patientservice.service;
+
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    private final PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getAllPatients() {
+        List<Patient> patients = patientRepository.findAll();
+
+        //Convert the list of Patient entities to a list of PatientResponseDTOs
+        // Map the list of Patient entities to a list of PatientResponseDTOs
+        // Using Java 8 Stream API to interate over the list of patients
+        // and convert each Patient entity to a PatientResponseDTO
+        List<PatientResponseDTO> patientResponseDTOS =
+
+                // Stream the list of patients
+                patients.stream()
+                        // Map each Patient entity to a PatientResponseDTO
+                .map(patient -> PatientMapper.toPatientResponseDTO(patient))
+                                .toList();
+        return patientResponseDTOS;
+
+
+
+
+    }
+}
